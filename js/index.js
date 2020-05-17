d866c3ecd129d0ce2c213b65f1921a8bc97e1544
alexknipfer <alexanderknipfer@gmail.com>
2020-05-16 22:51:58 -0400

    Load wasm module

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
-import("../pkg/index.js").catch(console.error);
+const wasm = import("../pkg/index.js");
+
+wasm.then((module) => {
+  const button = document.getElementById("parse");
+
+  button.addEventListener("click", () => {
+    const input = document.getElementById("markdown");
+    module.parse_input(input.value);
+  });
+});
